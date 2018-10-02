4ead1d50fd41f1d4e84f9985996c8c1ea5b30595
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-02 11:21:43 +0200

    enable hystrix

--- src/main/java/at/hochbichler/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/at/hochbichler/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,6 +1,7 @@
 package at.hochbichler.microservices.limitsservice;
 
 import at.hochbichler.microservices.limitsservice.bean.LimitsConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitsConfiguration retrieveLimitsFromConfiguration() {
         return new LimitsConfiguration(configuration.getMinimum(),configuration.getMaximum());
+    }
+
+    @GetMapping("/limits-fault")
+    @HystrixCommand(fallbackMethod = "retrieveLimitsFallback")
+    public LimitsConfiguration retrieveLimitsFault() {
+        throw new RuntimeException("A fault occured!");
+    }
+
+    public LimitsConfiguration retrieveLimitsFallback() {
+        return new LimitsConfiguration(1,100);
     }
 }
--- src/main/java/at/hochbichler/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/at/hochbichler/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,10 +2,13 @@ package at.hochbichler.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class LimitsServiceApplication {
 
     public static void main(String[] args) {
